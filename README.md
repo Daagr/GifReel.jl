7534a71b8eed2429e5ac4be5c612a06739165d92
Mikael Ilmanen <daagr.i@gmail.com>
2018-02-04 01:09:54 +0200

    Basics done.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 [![Coverage Status](https://coveralls.io/repos/Daagr/GifReel.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/Daagr/GifReel.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/Daagr/GifReel.jl/coverage.svg?branch=master)](http://codecov.io/github/Daagr/GifReel.jl?branch=master)
+
+GifReel.jl is a simple Reel.jl clone.
+
+Features / Differences:
+- Doesn't create files when used
+- Embeds gif in the document using data URI scheme
+- Doesn't call external programs
--- src/GifReel.jl
+++ src/GifReel.jl
@@ -1,5 +1,21 @@
 module GifReel
 
-# package code goes here
+using FileIO
+
+export showanimation, animate
+
+function showanimation(imgs)
+	buf = IOBuffer()
+	write(buf, "<img src=\"data:image/gif;base64,")
+	b64 = Base64EncodePipe(buf)
+	save(Stream(format"GIF", b64), imgs)
+	close(b64)
+	write(buf, "\"></img>")
+	HTML(String(take!(buf)))
+end
+
+showanimation(f::Function, frames) = showanimation([f(i) for i in 1:frames])
+
+animate(f; width=640, height=480, frames=20) = showanimation([f(x, y, t) for y in linspace(-1, 1, height), x in linspace(-width/height, width/height, width), t in linspace(0, 1, frames)])
 
 end # module
